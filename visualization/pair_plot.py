b07e5e8c374f5e8af8c66ba804281d4d65bc3f0c
timurci <timurcakmakoglu@gmail.com>
2024-11-20 15:38:06 +0300

    Updated pair_plot format for flake8

--- visualization/pair_plot.py
+++ visualization/pair_plot.py
@@ -3,6 +3,7 @@ from pandas import DataFrame, read_csv
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+
 def _parse_cmd_arguments() -> tuple[DataFrame, Namespace]:
     parser = ArgumentParser(
         prog="pair_plot",
@@ -12,7 +13,7 @@ def _parse_cmd_arguments() -> tuple[DataFrame, Namespace]:
     parser.add_argument('file_path',
                         help="path to csv file")
     parser.add_argument('-c', '--columns', nargs='+', required=True,
-                        help="Ex: -c col4 col2 col3, use '*' to view all columns")
+                        help="Ex: -c col3 col5, use '*' to view all columns")
     parser.add_argument('--hue', default=None,
                         help="name of a categorical column for color")
     parser.add_argument('-i', '--index', default=False,
@@ -23,16 +24,16 @@ def _parse_cmd_arguments() -> tuple[DataFrame, Namespace]:
 
     if "*" in args.columns:
         args.columns = data.select_dtypes(include=['number']).columns.to_list()
-    
+
     return data, args
 
-    
 
 def pair_plot(data: DataFrame,
               features: list[str],
               hue: str | None = None):
     """Set up a pair-plot in seaborn"""
     sns.pairplot(data, vars=features, hue=hue)
+
 
 def main():
     """Display a pair-plot from a csv file"""
@@ -46,6 +47,7 @@ def main():
     sns.set_theme()
     pair_plot(data, args.columns, args.hue)
     plt.show()
+
 
 if __name__ == "__main__":
     main()
