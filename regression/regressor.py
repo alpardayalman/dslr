d5a671cca52056c2e2e45793dbe3310b04de4c74
timurci <timurcakmakoglu@gmail.com>
2024-12-10 22:13:03 +0300

    regressor.py: completed fit and predict functions

--- regression/regressor.py
+++ regression/regressor.py
@@ -11,17 +11,20 @@ class RegressionModel(ABC):
     def __init__(self):
         self.intercept_ = None
         self.coefs_ = None
-        self.transform_info = None
 
     @abstractmethod
     def predict(self, X: np.ndarray) -> np.ndarray:
-        """Predict the outcome from features in X"""
+        """Predict the outcome from features in X
+
+        ABC checks for errors
+        """
         if self.coefs_ is None or self.intercept_ is None:
             raise AssertionError("weights are not trained")
+        if self.intercept_.shape[0] != X.shape[1]:
+            raise AssertionError("weight dimension is incompatible with input")
 
-    @abstractmethod
     def fit(self, X, y, **kwargs):
-        """Manage input/output variables and perform regression algorithm"""
+        """Perform regression algorithm"""
         if kwargs.get('batch') is None:
             self.gdescent(X, y, **kwargs)
         else:
@@ -57,12 +60,48 @@ class RegressionModel(ABC):
 
 class LogisticRegressor(RegressionModel):
     """Binary logistic regression model"""
+    def __init__(self):
+        super().__init__()
+        self.transform_info = None
+
+    def transform_output(self, y: pd.Series) -> np.ndarray:
+        dummies = pd.get_dummies(y, dtype=pd.Float32Dtype)
+
+        classes = dummies.columns
+        self.transform_info = {'name': y.name, 'classes': classes}
+
+        return dummies.iloc[:, 0].to_numpy()
+
+    def inv_transform_output(self, y_pred: np.ndarray) -> pd.Series:
+        if self.transform_info is None:
+            raise AssertionError("tranformation info is unavailable")
+
+        classes = self.transform_info["classes"]
+        estimates = [classes[0] if x > .5 else classes[1] for x in y_pred]
+
+        return pd.Series(estimates,
+                         name=self.transform_info["name"])
+
+    def fit(self, X, y, **kwargs):
+        X = X.to_numpy()
+        y = self.transform_output(y)
+
+        super().fit(X, y, **kwargs)
+
     def predict(self, X: np.ndarray) -> np.ndarray:
         """Sigmoid estimator function"""
-        return sigmoid(self.intercept_ + self.coefs_ @ X)
+        super().predict(X)
+        return sigmoid(self.intercept_ + X @ self.coefs_)
 
     def predict_class(self, X):
-        raise NotImplementedError
+        indices = X.index
+
+        y_pred = self.predict(X)
+        y_pred = self.inv_transform_output(y_pred)
+
+        y_pred.rename(index=indices, inplace=True)
+
+        return y_pred
 
     def cost(self, X, y):
         """Log-loss function"""
@@ -70,38 +109,56 @@ class LogisticRegressor(RegressionModel):
 
         to_change = y_pred == 1.
         y_pred[to_change] = 1. - y_pred[to_change]
-        
+
         loss_sum = np.log(y_pred).sum(axis=0)
-        
+
         return -(loss_sum / y.shape[0])
 
     def cost_gradient(self, X, y):
         """Log-loss function for sigmoid estimator"""
         y_diff = self.predict(X) - y
         y_diff = y_diff.T  # class in row(s), samples in columns
-        
-        gradient_intercept = y_diff.sum(axis=1)        
+
+        gradient_intercept = y_diff.sum(axis=1)
         gradient_coefs = y_diff @ X
 
         gradient = np.hstack((gradient_intercept, gradient_coefs))
         gradient /= X.shape[0]
 
-        return gradient
+        return gradient.T  # (thetas, classes)
 
 
 class OneVsAllRegressor(LogisticRegressor):
     """One-vs-all logistic regression model"""
+    def transform_output(self, y: pd.Series) -> np.ndarray:
+        dummies = pd.get_dummies(y, dtype=pd.Float32Dtype)
 
-    # TODO: Restruct cost function in LogisticRegressor
-    
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+        classes = dummies.columns
+        self.transform_info = {'name': y.name, 'classes': classes}
+
+        return dummies.to_numpy()
+
+    def inv_transform_output(self, y_pred: np.ndarray) -> pd.Series:
+        if self.transform_info is None:
+            raise AssertionError("tranformation info is unavailable")
+
+        classes = self.transform_info["classes"]
+        df = pd.DataFrame(y_pred, columns=pd.Index(classes))
+
+        return df.idxmax(axis=1).rename(self.transform_info["name"])
 
     def predict_tabular(self, X) -> pd.DataFrame:
-        raise NotImplementedError
+        indices = X.index
+        classes = self.transform_info["classes"]
 
-    def predict_class(self, X: np.ndarray) -> str:
-        raise NotImplementedError
+        y_pred = self.predict(X)
+        y_pred = self.inv_transform_output(y_pred)
+
+        y_pred.rename(index=indices, inplace=True)
+
+        return pd.DataFrame(y_pred,
+                            index=indices,
+                            columns=pd.Index(classes))
 
 
 def sigmoid(z):
