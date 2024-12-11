2c6758c071a3570ad00139e16d99d27b0127767e
timurci <timurcakmakoglu@gmail.com>
2024-12-11 18:22:55 +0300

    logreg_train now writes to csv

--- logreg_train.py
+++ logreg_train.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import pandas as pd
+import numpy as np
 
 from regression.regressor import OneVsAllRegressor
 
@@ -24,13 +25,24 @@ def _parse_cmd_arguments():
                         choices=["gradient", "sgradient"])
 
     args = parser.parse_args()
+    
     data = pd.read_csv(args.file_path, index_col=args.index)
 
     y = data[args.label]
     X = data.drop(columns=[args.label])
+    X = X.select_dtypes(include=['number'])
 
     return X, y, args
 
+def min_max_normalize(X, feature_range=(0, 1)):
+    """
+        min max normalizer klasik 
+    """
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+    X_std = (X - X_min) / (X_max - X_min)
+    X_scaled = X_std * (feature_range[1] - feature_range[0]) + feature_range[0]
+    return X_scaled, X_min, X_max
 
 def main():
     """Perform logistic regression on data from a csv file"""
@@ -41,15 +53,19 @@ def main():
         print(type(e).__name__, e, sep=": ")
         exit(1)
 
-    # apply data transformation on X...
-
+    X = X.replace(np.nan, 0)
+    X, _, _ = min_max_normalize(X)
+    
     reg = OneVsAllRegressor()
     reg.fit(X, y, alpha=args.alpha, max_itr=args.itrmax)
 
     classes = reg.transform_info["classes"]
-    weights = pd.DataFrame(reg.weights, columns=pd.Index([classes]))
+    weights = pd.DataFrame(reg.weights, columns=pd.Index(classes))
 
-    weights.to_csv("weights.csv")
+    print(weights)
+
+    name = reg.transform_info["name"]
+    weights.to_csv(name + "_weights.csv")
 
 
 if __name__ == "__main__":
